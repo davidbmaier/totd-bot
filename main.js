3b898581de07f7c7474be7c059066d216bb11f0a
David B. Maier <davidbmaier@gmail.com>
2021-02-07 16:03:56 +0100

    Enable custom emojis on other servers

--- main.js
+++ main.js
@@ -18,12 +18,12 @@ client.on('message', async (msg) => {
     const totd = await tmAPI.getCurrentTOTD(credentials);
     const message = await msg.channel.send(format.formatTOTDMessage(totd));
     const emojis = [
-      message.guild.emojis.cache.get('807983766239838208'),
-      message.guild.emojis.cache.get('807983738603962368'),
-      message.guild.emojis.cache.get('807983713698316308'),
-      message.guild.emojis.cache.get('807983669330706482'),
-      message.guild.emojis.cache.get('807983625001107497'),
-      message.guild.emojis.cache.get('807983052046598165')
+      client.emojis.resolve(`807983766239838208`),
+      client.emojis.resolve(`807983738603962368`),
+      client.emojis.resolve(`807983713698316308`),
+      client.emojis.resolve(`807983669330706482`),
+      client.emojis.resolve(`807983625001107497`),
+      client.emojis.resolve(`807983052046598165`)
     ];
     emojis.forEach(async (emoji) => {
       await message.react(emoji);
