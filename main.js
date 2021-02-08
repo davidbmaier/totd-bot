3cf55718c4a1f956a4cbaa0924a02b14f7f18060
David B. Maier <davidbmaier@gmail.com>
2021-02-08 21:07:52 +0100

    Add cron for scheduled posts

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const cron = require('cron');
 
 const tmAPI = require('./tmApi');
 const format = require('./format');
@@ -8,27 +9,39 @@ require('dotenv').config();
 
 const discordToken = process.env.DISCORD_TOKEN;
 
+const displayCurrentTOTD = async (channel) => {
+  console.log(`Sending current TOTD to ${channel.name} in ${channel.guild.name}`);
+
+  const credentials = await tmAPI.loginToTM();
+  const totd = await tmAPI.getCurrentTOTD(credentials);
+  const message = await channel.send(format.formatTOTDMessage(totd));
+  // add rating emojis
+  const emojis = [
+    client.emojis.resolve(`807983766239838208`),
+    client.emojis.resolve(`807983738603962368`),
+    client.emojis.resolve(`807983713698316308`),
+    client.emojis.resolve(`807983669330706482`),
+    client.emojis.resolve(`807983625001107497`),
+    client.emojis.resolve(`807983052046598165`)
+  ];
+  emojis.forEach(async (emoji) => {
+    await message.react(emoji);
+  });
+};
+
+// display the current totd every day at 19:00:30
+let scheduledTOTD = new cron.CronJob('30 00 19 * * *', async () => {
+  // TODO: get the channels this should post to
+  const channel = await client.channels.fetch('763052026028359690');
+  displayCurrentTOTD(channel);
+}).start();
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Ready as ${client.user.tag}!`);
 });
 
-client.on('message', async (msg) => {
-  if (msg.content === '!totd') {
-    const credentials = await tmAPI.loginToTM();
-    const totd = await tmAPI.getCurrentTOTD(credentials);
-    const message = await msg.channel.send(format.formatTOTDMessage(totd));
-    const emojis = [
-      client.emojis.resolve(`807983766239838208`),
-      client.emojis.resolve(`807983738603962368`),
-      client.emojis.resolve(`807983713698316308`),
-      client.emojis.resolve(`807983669330706482`),
-      client.emojis.resolve(`807983625001107497`),
-      client.emojis.resolve(`807983052046598165`)
-    ];
-    emojis.forEach(async (emoji) => {
-      await message.react(emoji);
-    });
-  }
+client.on('message', (msg) => {
+  // TODO: handle config messages
 });
 
 client.login(discordToken);
