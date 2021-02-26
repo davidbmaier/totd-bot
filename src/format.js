7b384fa176f2dda53dc9e721e351a42ff9915b65
David B. Maier <davidbmaier@gmail.com>
2021-02-26 22:40:29 +0100

    Use default font weight on prod

--- src/format.js
+++ src/format.js
@@ -305,7 +305,7 @@ const formatBingoBoard = async (fields) => {
   const background = await Canvas.loadImage(`./src/resources/bingoTemplate.png`);
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-  ctx.font = `18px ${fontName} medium`;
+  ctx.font = `18px ${fontName}`;
   ctx.textAlign = `center`;
   ctx.textBaseline = `middle`;
   ctx.fillStyle = `#FFFFFF`;
