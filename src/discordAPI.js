06fbee9a980eeb56576dde19ecc127b64b2184f3
David B. Maier <davidbmaier@gmail.com>
2021-04-28 19:33:56 +0200

    Trim track names for votes

--- src/discordAPI.js
+++ src/discordAPI.js
@@ -329,11 +329,8 @@ const updateTOTDReactionCount = async (reaction, add) => {
   const totdMessage = await redisAPI.getCurrentTOTD(redisClient);
 
   // it's possible there is no message in the redis cache, but that's a rare edge case (in which reactions won't be recorded)
-  const currentTrackName = totdMessage?.embed?.fields.find((field) => field.name === `Name`)?.value;
-  const reactionTrackName = reaction.message?.embeds[0]?.fields.find((field) => field.name === `Name`)?.value;
-  console.log(`reaction debug:`);
-  console.log(`stored totd title:      "${currentTrackName}"`);
-  console.log(`reaction message title: "${reactionTrackName}"`);
+  const currentTrackName = totdMessage?.embed?.fields.find((field) => field.name === `Name`)?.value.trim();
+  const reactionTrackName = reaction.message?.embeds[0]?.fields.find((field) => field.name === `Name`)?.value.trim();
   if (currentTrackName === reactionTrackName) {
     const ratingEmojis = constants.ratingEmojis;
     for (let i = 0; i < ratingEmojis.length; i++) {
