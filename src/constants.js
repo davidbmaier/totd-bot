2822d3ac23d4963b02240f5ca56c595b00f95af5
David B. Maier <davidbmaier@gmail.com>
2021-08-08 22:33:50 +0200

    Remove author-specific bingo fields

--- src/constants.js
+++ src/constants.js
@@ -53,9 +53,6 @@ const bingoFields = [
   // author
   `Author's\nfirst TOTD`,
   `Author already\nhad TOTD\nin the last 7 days`,
-  `Map by a\nTMGL player`,
-  `Map by htimh`,
-  `Map by priez`,
   // times
   `Author time\nover a minute`,
   `Author time\nunder 30s`,
