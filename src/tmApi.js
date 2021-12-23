cc050bf989ac297fe13dcc84f42b385db171d04f
David B. Maier <davidbmaier@gmail.com>
2021-12-23 16:47:27 +0100

    Restructure login and leaderboard requests

--- src/tmApi.js
+++ src/tmApi.js
@@ -14,14 +14,14 @@ const uplayLogin = Buffer.from(process.env.USER_LOGIN).toString(`base64`);
 
 const loginToTM = async () => {
   try {
-    const ubi = await loginUbi(uplayLogin); // login to ubi, level 0
-    const tmUbi = await loginTrackmaniaUbi(ubi.ticket); // login to trackmania, level 1
-    const nadeo = await loginTrackmaniaNadeo(tmUbi.accessToken, `NadeoLiveServices`); // login to nadeo, level 2
+    const ubi = await loginUbi(uplayLogin); // login to ubi
+    const tmUbi = await loginTrackmaniaUbi(ubi.ticket); // login to trackmania
+    const nadeo = await loginTrackmaniaNadeo(tmUbi.accessToken, `NadeoLiveServices`); // login to nadeo
 
     return {
-      level0: ubi.ticket,
-      level1: tmUbi.accessToken,
-      level2: nadeo.accessToken
+      ubiTicket: ubi.ticket,
+      tmToken: tmUbi.accessToken,
+      nadeoToken: nadeo.accessToken
     };
   } catch (e) {
     console.log(`loginToTM error:`);
@@ -29,15 +29,24 @@ const loginToTM = async () => {
   }
 };
 
+const getNadeoHeaders = (credentials) => {
+  return {
+    'Content-Type': `application/json`,
+    'Ubi-AppId': `86263886-327a-4328-ac69-527f0d20a237`,
+    'Ubi-RequestedPlatformType': `uplay`,
+    'Authorization': `nadeo_v1 t=${credentials.nadeoToken}`
+  };
+};
+
 const getPlayerName = async (credentials, playerId) => {
   try {
-    const tmProfiles = await getProfiles(credentials.level1, [playerId]);
+    const tmProfiles = await getProfiles(credentials.tmToken, [playerId]);
 
     if (tmProfiles.length === 0) {
       return playerId;
     }
 
-    const ubiProfiles = await getProfilesById(credentials.level0, [tmProfiles[0].uid]);
+    const ubiProfiles = await getProfilesById(credentials.ubiTicket, [tmProfiles[0].uid]);
 
     if (ubiProfiles.profiles.length === 0) {
       return playerId;
@@ -91,7 +100,7 @@ const getTMXInfo = async (mapUid) => {
 
 const getCurrentTOTD = async (credentials) => {
   try {
-    const totds = await getTOTDs(credentials.level2);
+    const totds = await getTOTDs(credentials.nadeoToken);
 
     let currentTOTDMeta;
     for (let i = 0; i < totds.monthList[0].days.length; i++) {
@@ -103,7 +112,7 @@ const getCurrentTOTD = async (credentials) => {
       }
     }
 
-    const currentTOTDArray = await getMaps(credentials.level1, [currentTOTDMeta.mapUid]);
+    const currentTOTDArray = await getMaps(credentials.tmToken, [currentTOTDMeta.mapUid]);
     const currentTOTD = currentTOTDArray[0];
     currentTOTD.seasonUid = currentTOTDMeta.seasonUid;
 
@@ -150,73 +159,56 @@ const getCurrentTOTD = async (credentials) => {
   }
 };
 
+const getLeaderboardPosition = async (credentials, seasonUid, mapUid, position) => {
+  try {
+    const route = `https://live-services.trackmania.nadeo.live/api/token/leaderboard/group/${seasonUid}/map/${mapUid}/top?length=1&onlyWorld=true&offset=${position - 1}`;
+
+    const response = await axios.get(route, {
+      headers: getNadeoHeaders(credentials)
+    });
+
+    const record = response?.data?.tops[0]?.top[0];
+
+    if (record) {
+      record.playerName = await getPlayerName(credentials, record.accountId);
+      record.position = position;
+    }
+
+    return record;
+  } catch (e) {
+    console.log(`getLeaderboardPosition error:`);
+    console.log(e);
+  }
+};
+
 const getTOTDLeaderboard = async (credentials, seasonUid, mapUid) => {
   try {
-    const headers = {
-      'Content-Type': `application/json`,
-      'Ubi-AppId': `86263886-327a-4328-ac69-527f0d20a237`,
-      'Ubi-RequestedPlatformType': `uplay`,
-      'Authorization': `nadeo_v1 t=${credentials.level2}`
-    };
+    const headers = getNadeoHeaders(credentials);
 
-    const baseRoute = `https://live-services.trackmania.nadeo.live/api/token/leaderboard/group/${seasonUid}/map/${mapUid}/top?length=50&onlyWorld=true`;
+    const baseRoute = `https://live-services.trackmania.nadeo.live/api/token/leaderboard/group/${seasonUid}/map/${mapUid}/top?length=10&onlyWorld=true`;
     const baseResponse = await axios.get(baseRoute, {
       headers,
     });
 
     const leaderboard = baseResponse?.data;
-    const records = leaderboard.tops[0].top.slice(0, 10); // get top 10 records
-
-    const top100Route = `${baseRoute}&offset=99`;
-    const top100Response = await axios.get(top100Route, {
-      headers,
-    });
-    let top100;
-    const top100Records = top100Response?.data?.tops[0]?.top;
-    if (top100Records && top100Records[0]) {
-      top100 = top100Records[0];
-    }
-
-    const top1000Route = `${baseRoute}&offset=999`;
-    const top1000Response = await axios.get(top1000Route, {
-      headers,
-    });
-    let top1000;
-    const top1000Records = top1000Response?.data?.tops[0]?.top;
-    if (top1000Records && top1000Records[0]) {
-      top1000 = top1000Records[0];
-    }
-
-    const top10000Route = `${baseRoute}&offset=9999`;
-    const top10000Response = await axios.get(top10000Route, {
-      headers,
-    });
-    let top10000;
-    const top10000Records = top10000Response?.data?.tops[0]?.top;
-    if (top10000Records && top10000Records[0]) {
-      top10000 = top10000Records[0];
-    }
+    const records = leaderboard.tops[0].top;
 
     for (let i = 0; i < records.length; i++) {
       records[i].playerName = await getPlayerName(credentials, records[i].accountId);
       records[i].position = i + 1;
     }
 
+    const top100 = await getLeaderboardPosition(credentials, seasonUid, mapUid, 100);
+    const top1000 = await getLeaderboardPosition(credentials, seasonUid, mapUid, 1000);
+    const top10000 = await getLeaderboardPosition(credentials, seasonUid, mapUid, 10000);
+
     if (top100) {
-      top100.position = 100;
-      top100.playerName = await getPlayerName(credentials, top100.accountId);
       records.push(top100);
     }
-
     if (top1000) {
-      top1000.position = 1000;
-      top1000.playerName = await getPlayerName(credentials, top1000.accountId);
       records.push(top1000);
     }
-
     if (top10000) {
-      top10000.position = 10000;
-      top10000.playerName = await getPlayerName(credentials, top10000.accountId);
       records.push(top10000);
     }
 
